c7aa4c2a28ef85b1f660eff50b02089b9e9a6343
ekibru <kibru4204@gmail.com>
2022-08-04 19:38:06 -0500

    Update InsufficientFunds.java

--- src/com/ephrem/InsufficientFunds.java
+++ src/com/ephrem/InsufficientFunds.java
@@ -2,10 +2,9 @@ package com.ephrem;
 
 import javax.swing.*;
 /**  Atm Machine programing
- *  Group Member
+
  * Ephrem Kidane
- * Jean Amani Paul
- * Suzan Gehad
+
  */
 
 public class InsufficientFunds extends Exception{
